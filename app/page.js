6048448841ac11ae6417b501a692eb9ee197b04b
Laurenda <laurenda.tessi@epitech.eu>
2026-07-29 23:17:31 +0200

    fix :: erreur de page

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Navbar from '@/components/navbar';
 export default function Home() {
   return (
     <div className="bg-white">
+      <Navbar />
       <div className="relative isolate px-6 lg:px-8">
         <div
           aria-hidden="true"
